65da11441bce6d57b453066212533cb9ce0d0838
susannaholubiyi <susannaholubiyi@gmail.com>
2024-04-24 16:30:26 +0100

    question 1-5

--- testScores.js
+++ testScores.js
@@ -35,23 +35,16 @@ function findAfternoonClassesFrom(classTime){
 }
 
 function getTotalExpense(expenses){
+    let totalExpense = 0
+for (const expense in expenses){
+    totalExpense += expense
 
+}
 }
 
 // function exmple(){
 //     let num = [12,3,4,5,9,4,5]
 // num.splice(2,0,2,3);
-// console.log(num);
-// let array = [1, 2, 3, 4, 5, 6]
-// array.forEach((num) => {console.log(num * 2);})
-// let arr = []
 
-// let answer = array.forEach((num)=>{arr.push(num * 2);})
-// let ans = array.map((num) => num % 2 == 1)
-// let answerr = array.filter(num => num % 2 == 0).map(num => num * 10)
-// console.log(arr)
-// console.log(ans)
-// console.log(answerr)
-// }
 
 module.exports = {getPassScoreFrom, increaseScoresByFive, getTheSquareOf, assignBookTo, findAfternoonClassesFrom, getTotalExpense}
